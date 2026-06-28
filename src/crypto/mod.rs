fce75163b04b5304e67cc16786e564bb5ce9eb19
Alexey <247128645+axkurcom@users.noreply.github.com>
2026-06-28 15:45:53 +0300

    Rustfmt

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -7,7 +7,5 @@ pub mod random;
 pub use aes::{AesCbc, AesCtr};
 #[allow(unused_imports)]
 pub use hash::build_middleproxy_prekey;
-pub use hash::{
-    crc32, crc32c, derive_middleproxy_keys, sha256, sha256_hmac,
-};
+pub use hash::{crc32, crc32c, derive_middleproxy_keys, sha256, sha256_hmac};
 pub use random::SecureRandom;
